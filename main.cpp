38f9ec6b70979fd5b386b019d59e809a61d76981
padiwisdom <42818702+padiwisdom@users.noreply.github.com>
2018-08-29 13:59:28 -0700

    Add files via upload
    c++ assignment

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num; n, coun=0;
+    cout <<" Enter a number which has only two factors meaning it is a prime or not prime ";
+    cin >> num;
+    if ( num ==0 )
+    {
+        cout << num <<" is not prime";
+    }
+    else
+    for( n =2; n<num; n++)
+    if(num % n ==0)
+    count ++
+}
+if (cout<< num <<" is not prime")
+else
+cout << num <<"is prime";
+    return 0;
+}
