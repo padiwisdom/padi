f0642e8e8849350e453de4ebd989e79fdd907814
padiwisdom <42818702+padiwisdom@users.noreply.github.com>
2018-09-02 18:07:56 -0700

    Add files via upload
    CSCD 205

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+float a;
+    cout << "Enter your Score to know your grade";
+    cin>>a;
+
+    if(a >= 80 && a <= 100)
+    {
+       cout << "GRADE : A ";
+    }
+
+    else if(a >= 75  && a <= 79)
+    {
+       cout << "GRADE : B+ ";
+    }
+
+    else if(a >= 70 && a <= 74)
+    {
+       cout << "GRADE : B ";
+    }
+
+    else if(a >= 65 && a <= 69)
+    {
+       cout << "GRADE : C+ ";
+    }
+
+    else if(a >= 60 && a <= 64)
+    {
+       cout << "GRADE : C ";
+    }
+
+    else if(a >= 55 && a <= 59)
+    {
+       cout << "GRADE : D+ ";
+    }
+
+    else if(a >= 50 && a <= 54)
+    {
+       cout << "GRADE : D ";
+    }
+
+    else if(a >= 45 && a <= 49)
+    {
+       cout << "GRADE : E ";
+    }
+
+    else if(a >= 0 && a <= 44)
+    {
+       cout << "GRADE : F ";
+    }
+
+    else
+    {
+       cout << "No  grading found for your score ";
+    }
+
+    map();
+    return 0;
+
+
+}
